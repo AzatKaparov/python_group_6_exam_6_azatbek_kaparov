97bc02f2dcaf42c2a18d5b2f005caf214e9dd7ec
Azat Kaparov <azatkaparov843@gmail.com>
2020-08-01 13:15:59 +0600

    Изменения в views.py и urls.py Добавление style.css, базового шаблона и index.html (Этап 2)

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Guestbook
+from django.http import HttpResponseNotFound, HttpResponseNotAllowed
 
-# Create your views here.
+# В index добавлен фильтр
+
+
+def index_view(request):
+    all = Guestbook.objects.all().order_by('-created_date')
+    data = []
+    for i in all:
+        if i.status =='active':
+            data.append(i)
+        else:
+            pass
+    return render(request, 'index.html', context={
+        'guests': data
+    })
