c6a95b1f194f5f1dd9821d42bbe2a5adb9055085
Azat Kaparov <azatkaparov843@gmail.com>
2020-08-01 12:02:16 +0600

    Регистрация модели Доработка админки

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Guestbook
 
-# Register your models here.
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'status', 'created_date', 'redacted_date')
+    fields = ['name', 'email', 'status', 'text']
+    list_filter = ('status',)
+    search_fields = ('name',)
+
+
+admin.site.register(Guestbook, GuestAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+DEFAULT_STATUS = 'active'
+
+STATUS_CHOICE = [
+    (DEFAULT_STATUS, 'Активно'),
+    ('blocked', 'Заблокировано')
+]
+
+
+class Guestbook(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Имя автора записи')
+    email = models.EmailField(null=False, blank=False, verbose_name='Почта автора записи')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст записи')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Время создания записи')
+    redacted_date = models.DateTimeField(auto_now=True, verbose_name='Время последнего редактирования')
+    status = models.CharField(max_length=30, choices=STATUS_CHOICE, verbose_name='Статус')
+
+    def __str__(self):
+        return f'{self.pk}: {self.name} - {self.status}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
